b302f5c6ae94b5db36a32e86aed131e005ac5585
Michael Cerasi <michaelcerasi@hotmail.com>
2018-11-07 20:52:02 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,13 +8,14 @@ def roll_call_dwarves(array)
 end
 roll_call_dwarves(dwarves)
 
-  planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
+planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
 def summon_captain_planet(array)
   nu_array = []
+  i = 0
   while i < array.length
     nu_array << array[i].capitalize + "!"
     i += 1
-end
+  end
  nu_array
 end
 summon_captain_planet(planeteer_calls)
