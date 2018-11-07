9bcb1e40ebf9cb6e1b8a339e3284f0f965daddca
Michael Cerasi <michaelcerasi@hotmail.com>
2018-11-07 20:22:40 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,16 +9,9 @@ end
 roll_call_dwarves(dwarves)
 
 planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
-def summon_captain_planet(array)
-  nu_array = []
-  i = 0
-  while i < array.length
-    nu_array << array[i].capitalize + "!"
-    i += 1
-  end
- nu_array
+def summon_captain_planet (planeteer_calls)
+  string.split.map(&:capitalize).join('earth, wind, fire, water, heart')
 end
-summon_captain_planet(planeteer_calls)
 
 def long_planeteer_calls(calls)
   calls.any? do |x|
