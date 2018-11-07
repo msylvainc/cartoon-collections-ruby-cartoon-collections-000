9fc55e8cc482a97668c8b356348e2b8303f73670
Michael Cerasi <michaelcerasi@hotmail.com>
2018-11-07 21:04:37 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,10 +2,11 @@ dwarves = ["Doc", "Dopey", "Bashful", "Grumpy", "Sneezy", "Sleepy", "Happy"]
 def roll_call_dwarves(array)
   i = 0
   while i < array.length
-    puts "#{i + 1}. #{array[i]}"
+    puts "#{i + 1}. #{array}"
     i += 1
   end
 end
+
 roll_call_dwarves(dwarves)
 
 planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
@@ -20,9 +21,16 @@ def summon_captain_planet(array)
 end
 summon_captain_planet(planeteer_calls)
 
+def long_planeteer_calls(calls)
+  calls.any? do |x|
+    x.length > 4
+  end
+end
+
   def find_the_cheese(strings)
     cheese_types = ["cheddar", "gouda", "camembert"]
     strings.find do |x|
       cheese_types.include?(x)
     end
 end
+find_the_cheese(cheese_types)
