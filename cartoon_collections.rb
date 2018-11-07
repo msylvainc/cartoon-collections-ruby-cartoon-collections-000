88272db00eb1747e6668c47b1d2c66965725613c
Michael Cerasi <michaelcerasi@hotmail.com>
2018-11-07 20:56:53 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,8 +10,9 @@ roll_call_dwarves(dwarves)
 
 planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
 def summon_captain_planet(array)
-planeteer_calls.map! {|x| array.capitalize + "!"}
+planeteer_calls.map! {|array| array.capitalize + "!"}
 end
+summon_captain_planet(planeteer_calls)
 
 def long_planeteer_calls(calls)
   calls.any? do |x|
