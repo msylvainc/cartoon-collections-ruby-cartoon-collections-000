32ef8cd5d2c6d49c2d01fb27c453157068669d87
Michael Cerasi <michaelcerasi@hotmail.com>
2018-11-07 21:02:16 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,16 +9,16 @@ end
 roll_call_dwarves(dwarves)
 
 planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
-def summon_captain_planet (array)
-planeteer_calls.map {|x|} {array.capitalize + "!"}
+def summon_captain_planet(array)
+  nu_array = []
+  i = 0
+  while i < array.length
+    nu_array << array[i].capitalize + "!"
+    i += 1
+  end
+ nu_array
 end
 summon_captain_planet(planeteer_calls)
-
-def long_planeteer_calls(calls)
-  calls.any? do |x|
-    x.length > 4
-  end
-end
 
   def find_the_cheese(strings)
     cheese_types = ["cheddar", "gouda", "camembert"]
